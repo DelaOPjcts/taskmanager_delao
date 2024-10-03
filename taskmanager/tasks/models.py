03974bf47ae61b22b7fca0b4b8cf355389579e0d
Daniel <ddelao.cs@outlook.com>
2024-10-03 14:00:46 -0600

    due_date in models

--- taskmanager/tasks/models.py
+++ taskmanager/tasks/models.py
@@ -1,12 +1,18 @@
 # tasks/models.py
 
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta  # Importar timedelta desde datetime
+
+def get_due_date():
+    return timezone.now().date() + timedelta(days=1)
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateField(default=get_due_date)  # Usar la función
     
     def __str__(self):
         return self.title
